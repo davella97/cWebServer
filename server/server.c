b5f0ef4d98ad58dbc34643cb9873bf5cd746fdbd
davella97 <davidevella14@gmail.com>
2025-09-26 20:09:20 +0200

    add connection handler and request resolver

--- server/server.c
+++ server/server.c
@@ -7,8 +7,53 @@
 #include <unistd.h>
 
 #define PORT 8080
+#define MAX_REQUEST_SIZE 8192
 
-void *connectionHandler(void *arg) {}
+short int read_line(char *buffer, int client) {
+  int pos = 0;
+  short n;
+  int len = 0;
+  char c;
+
+  while (pos < MAX_REQUEST_SIZE - 1) {
+    n = read(client, &c, 1);
+    len += n;
+
+    if (n <= 0)
+      break;
+    buffer[pos++] = c;
+
+    if (pos >= 2 && buffer[pos - 2] == '\r' && buffer[pos - 1] == '\n') {
+      break;
+    }
+  }
+
+  return len;
+}
+
+void *connectionHandler(void *arg) {
+  int client = *((int *)arg);
+  char method[8];
+  char path[1024];
+  char version[12];
+  char buffer[MAX_REQUEST_SIZE] = {0};
+
+  short int buffLen = read_line(buffer, client);
+  printf("%s\n", buffer);
+  buffer[buffLen] = '\0';
+
+  if (sscanf(buffer, "%7s %1023s %11s", method, path, version) == 3) {
+    fprintf(stderr, "Method : %s\nPath : %s\nVersion :%s", method, path,
+            version);
+    if (strcmp(method, "GET") != 0) {
+      //...
+    } else {
+      printf("Unsupported request");
+    }
+  } else {
+    printf("400 Bad Request");
+  }
+}
 
 int main() {
   int server_fd, new_socket;
